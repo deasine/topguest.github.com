094abb3b55911c7fedeb29c0633e2b63c6139c0d
Nathan McGinness <nathanmcginness@gmail.com>
2011-04-11 19:32:50 -0700

    resolved ugly js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
 $(document).ready(function () {		
-		
-	// Being really dodgy...
-	$(".image-link").live({
-      mouseenter:
-         function()
-         {
-				$(this).next().hide();
-         },
-      mouseleave:
-         function()
-         {
-				$(this).next().show();
-         }
-     }
-  );
+	
 });
