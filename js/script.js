6d128d53480b73506f386395433151d37c3ee057
Nathan McGinness <nathanmcginness@gmail.com>
2011-04-11 19:11:35 -0700

    display logic for posts on homepage in place. note, some really dodgy js on image hovers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$(function(){
-
-});
+$(document).ready(function () {		
+		
+	// Being really dodgy...
+	$(".image-link").live({
+      mouseenter:
+         function()
+         {
+				$(this).next().hide();
+         },
+      mouseleave:
+         function()
+         {
+				$(this).next().show();
+         }
+     }
+  );
+});
